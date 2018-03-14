08d486313aed0d90bb8fac5b6c229f8b103d2261
Santiago Giraldo <videojuegosjuanda666@gmail.com>
2018-03-14 12:02:28 -0500

    musical museum Vr

---
+++ Assets/Scripts/DisplayFps.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DisplayFps : MonoBehaviour {
+
+    public Text text;
+    public float fps;
+    public float deltaTime;
+	
+	void Start ()
+    {
+        deltaTime = 0.0f;
+		
+	}
+	
+
+	void Update ()
+    {
+        fps = Mathf.FloorToInt(1.0f / deltaTime);
+        text.text = fps.ToString();
+
+        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
+
+	}
+}
---
+++ Assets/Scripts/Listener.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Listener : MonoBehaviour
+{
+    public AudioSource audi;
+    public AudioClip audiclp;
+    [SerializeField]
+    private float vol = 1f;
+
+
+    void Awake()
+    {
+        audi = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        audi.Stop();
+        audi.PlayOneShot(audiclp, vol);
+        print("deberia sonar");
+
+    }
+
+
+   private void  OnTriggerExit(Collider Other)
+    {
+        audi.Stop();
+
+
+    }
+}
